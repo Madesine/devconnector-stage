f8e950b1ceedfcc85c4af7df07218ebc7c0dafc4
Madesine <kurila28@gmail.com>
2020-06-06 15:48:02 +0300

    Add base routes & Schema for User

---
+++ routes/api/users.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+
+// @route GET api/users
+// @desc Test
+// @access Public
+router.get("/", (req, res) => {
+	res.send("Hello i'm GET 'api/users'");
+});
+
+module.exports = router;
