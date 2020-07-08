26bd7c984ae1c6cd41b6c594a4ae4e9a7f66148c
Madesine <kurila28@gmail.com>
2020-07-09 01:16:01 +0300

    Add get user route api/auth

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const router = express.Router();
 
+const User = require("../../models/User");
+const authMiddlware = require("../../middlewares/auth");
+
 // @route GET api/auth
 // @desc Get user
 // @access Private
-router.get("/", (req, res) => {
-	
+router.get("/", authMiddlware, async (req, res) => {
+	try {
+		const user = await User.findById(req.user.id).select('-password');
+		res.json({ user });
+	} catch (err) {
+		console.err(err.message);
+		res.status(500).send("Server error");
+	}
 });
 
 module.exports = router;
