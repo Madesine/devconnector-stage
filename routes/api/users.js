d726d46938ac9b801640d0e01cf53e5281403b17
Madesine <kurila28@gmail.com>
2020-07-09 00:35:44 +0300

    Add registration route api/users

--- routes/api/users.js
+++ routes/api/users.js
@@ -32,7 +32,46 @@ router.post(
 
 		try {
 			let user = await User.findOne({ email });
-		} catch (e) {}
+
+			if (user) {
+				return res
+					.status(400)
+					.json({ errors: [{ msg: "User already exists" }] });
+			}
+
+			const avatar = gravatar.url(email, {
+				s: "200",
+				r: "pg",
+				d: "mm",
+			});
+
+			user = new User({ name, email, avatar, password });
+
+			const salt = await bcrypt.genSalt(10);
+			user.password = await bcrypt.hash(password, salt);
+
+			await user.save();
+
+			const payload = {
+				user: {
+					id: user.id,
+				},
+			};
+
+			jwt.sign(
+				payload,
+				config.get("jwtSecret"),
+				{ expiresIn: 36000 },
+				(err, token) => {
+					if (err) throw err;
+					res.json({ token });
+				}
+			);
+		} catch (e) {
+			console.log(err.message);
+
+			res.status(500).send("Send error");
+		}
 	}
 );
 
