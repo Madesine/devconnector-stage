69d42125d483c7df8aed142d60a790de5c71aaa0
Madesine <kurila28@gmail.com>
2020-07-22 20:49:53 +0300

    Add experience route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -160,4 +160,48 @@ router.delete("/", authMiddleware, async (req, res) => {
 	}
 });
 
+// @route PATCH api/profile/experience
+// @desc Add profile experience
+// @access Private
+router.patch(
+	"/experience",
+	[
+		authMiddleware,
+		[
+			check("title", "Title is required").not().isEmpty(),
+			check("company", "Company is required").not().isEmpty(),
+			check("from", "From date is required").not().isEmpty(),
+		],
+	],
+	async (req, res) => {
+		const errors = validationResult(req);
+
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+
+		const newExp = ({
+			title,
+			company,
+			location,
+			from,
+			to,
+			current,
+			description,
+		} = req.body);
+
+		try {
+			const profile = await Profile.findOne({ user: req.user.id });
+
+			profile.experience.unshift(newExp);
+
+			await profile.save();
+			res.json(profile);
+		} catch (err) {
+			console.log(err.message);
+			res.status(500).send("Server error");
+		}
+	}
+);
+
 module.exports = router;
