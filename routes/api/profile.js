64c9c8a368acec11bd3c76fde2bdda71c8ddbff4
Madesine <kurila28@gmail.com>
2020-07-22 21:16:43 +0300

    Add education patch route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -229,4 +229,49 @@ router.delete("/experience/:exp_id", authMiddleware, async (req, res) => {
 	}
 });
 
+// @route PATCH api/profile/education
+// @desc Add profile education
+// @access Private
+router.patch(
+	"/education",
+	[
+		authMiddleware,
+		[
+			check("school", "School is required").not().isEmpty(),
+			check("degree", "degree is required").not().isEmpty(),
+			check("from", "From date is required").not().isEmpty(),
+			check("fieldofstudy", "Field of study is required").not().isEmpty(),
+		],
+	],
+	async (req, res) => {
+		const errors = validationResult(req);
+
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+
+		const newEdu = ({
+			school,
+			degree,
+			fieldofstudy,
+			from,
+			to,
+			current,
+			description,
+		} = req.body);
+
+		try {
+			const profile = await Profile.findOne({ user: req.user.id });
+
+			profile.education.unshift(newEdu);
+
+			await profile.save();
+			res.json(profile);
+		} catch (err) {
+			console.log(err.message);
+			res.status(500).send("Server error");
+		}
+	}
+);
+
 module.exports = router;
