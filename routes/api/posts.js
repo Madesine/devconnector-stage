56c212f479b3d24b62903434e204d6e590dfff8d
Madesine <kurila28@gmail.com>
2020-07-23 02:10:36 +0300

    Add delete a post by id route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -75,4 +75,32 @@ router.get("/:id", authMiddleware, async (req, res) => {
 	}
 });
 
+// @route DELETE api/posts/:id
+// @desc Delete a post by id
+// @access Private
+router.delete("/:id", authMiddleware, async (req, res) => {
+	try {
+		const post = await Post.findById(req.params.id);
+
+		if (!post) {
+			return res.status(404).json({ msg: "Post not found" });
+		}
+
+		if (post.user.toString() !== req.user.id) {
+			return res.status(401).json({ msg: "User not authorized" });
+		}
+
+		await post.remove();
+		res.json({ msg: "Post has been successfully deleted" });
+	} catch (err) {
+		console.error(err.message);
+
+		if (err.kind === "ObjectId") {
+			return res.status(404).json({ msg: "Post not found" });
+		}
+
+		res.status(500).send("Server error");
+	}
+});
+
 module.exports = router;
