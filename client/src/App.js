b542a1ac0ec1e9ba01a3f2ddfa22661e7dc99713
Madesine <kurila28@gmail.com>
2020-07-24 00:26:31 +0300

    First steps into react add landing&navbar components

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import Landing from "./components/layout/Landing";
+import Navbar from "./components/layout/Navbar";
+import "./App.css";
+
+function App() {
+	return (
+		<>
+			<Navbar />
+			<Landing />
+		</>
+	);
+}
+
+export default App;
