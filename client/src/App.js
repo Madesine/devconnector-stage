f15e660c3ab8b41774c37ce855750871de0699cf
Madesine <kurila28@gmail.com>
2020-07-24 00:43:52 +0300

    Add routing for login & register components

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,24 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Landing from "./components/layout/Landing";
 import Navbar from "./components/layout/Navbar";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
 import "./App.css";
 
 function App() {
 	return (
-		<>
+		<Router>
 			<Navbar />
-			<Landing />
-		</>
+			<Route exact path="/" component={Landing} />
+			<section className="container">
+				<Switch>
+					<Route exact path="/register" component={Register}></Route>
+					<Route exact path="/login" component={Login}></Route>
+				</Switch>
+			</section>
+		</Router>
 	);
 }
 
